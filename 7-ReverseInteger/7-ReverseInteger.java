7c503b3ed261b06bd851d19da0a1f141ffde10de
Anushree S S <anushreesuresh11@gmail.com>
2026-07-09 09:17:20 +0530

    Create 7-ReverseInteger

---
+++ 7-ReverseInteger/7-ReverseInteger.java
@@ -0,0 +1,18 @@
+// Last updated: 7/9/2026, 9:17:18 AM
+public class Solution {
+    public static int reverse(int x) {
+        int ans = 0;
+
+        while (x != 0) {
+            int digit = x % 10;
+            x /= 10;
+
+            if (ans > Integer.MAX_VALUE / 10 || (ans == Integer.MAX_VALUE / 10 && digit > 7)) return 0;
+            if (ans < Integer.MIN_VALUE / 10 || (ans == Integer.MIN_VALUE / 10 && digit < -8)) return 0;
+
+            ans = ans * 10 + digit;
+        }
+
+        return ans;
+    }
+}
