8fb995e8716ef26a658f38c680b41a4c09acd1f8
Anushree S S <anushreesuresh11@gmail.com>
2026-07-09 09:14:13 +0530

    Create 1468-CheckIfNAndItsDoubleExist

---
+++ 1468-CheckIfNAndItsDoubleExist/1468-CheckIfNAndItsDoubleExist.java
@@ -0,0 +1,22 @@
+// Last updated: 7/9/2026, 9:14:11 AM
+class Solution {
+    public boolean checkIfExist(int[] arr) {
+        Arrays.sort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            int target = 2 * arr[i];
+            int lo = 0, hi = arr.length - 1;
+            while (lo <= hi) {
+                int mid = lo + (hi - lo) / 2;
+                if (arr[mid] == target && mid != i) 
+                    return true;
+                if (arr[mid] < target) 
+                    lo = mid + 1;
+                else 
+                    hi = mid - 1;
+            }
+        }
+
+        return false;
+    }
+}
+
